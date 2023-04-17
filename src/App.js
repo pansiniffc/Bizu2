84bd74700de1b636a2b2bbacb572752e84eaba7a
Renan Pansini <renanpansini15@gmail.com>
2023-04-17 19:20:32 -0400

    estrutura inicial

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import AddQuestao from './components/AddQuestao';
 import Questoes from './components/Questoes';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import Login from './components/Login/Login';
 
 function App() {
   return (
     <div>
-      <Questoes />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
